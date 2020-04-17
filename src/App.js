bf5ddb53df9ece7a07b989b69f99f9dc4fecd4b5
Trent Frederick <trentfrederick@gmail.com>
2020-04-17 18:45:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading:true});
+
+  //   const res = await axios.get('https://api.github.com/users');
+
+  //   this.setState({users: res.data, loading:false})
+
+  // }
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    let url = "https://api.github.com/search/users?q=" + text;
+    const res = await axios.get(url);
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ?true : false}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
